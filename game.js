6b6a1fd3ce896a57bcb950c0c0c7b50a741d4f0a
rmrotek <remek87@gmail.com>
2018-09-27 14:15:50 +0200

    key listeners implemented

---
+++ game.js
@@ -0,0 +1,38 @@
+var tank = document.querySelector('#tank');
+var position = 0;
+var acceleration = 0;
+
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'KeyW') {
+        forwardPressed = true;
+    }
+    if (event.code === 'KeyS') {
+        backwardsPressed = true;
+    }
+    if (event.code === 'KeyA') {
+        rotateLeft = true;
+    }
+    if (event.code === 'KeyD') {
+        rotateRight = true;
+    }
+    if (event.code === 'KeyR') {
+        turretRotateToggle = true;
+    }
+})
+window.addEventListener('keyup', function () {
+    if (event.code === 'KeyW') {
+        forwardPressed = false;
+    }
+    if (event.code === 'KeyS') {
+        backwardsPressed = false;
+    }
+    if (event.code === 'KeyA') {
+        rotateLeft = false
+    }
+    if (event.code === 'KeyD') {
+        rotateRight = false;
+    }
+    if (event.code === 'KeyR') {
+        turretRotateToggle = false;
+    }
+})
